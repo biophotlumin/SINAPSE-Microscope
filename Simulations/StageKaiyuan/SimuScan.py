d2128603312fa373c40277566c376e4a6d31497d
FSemmer <florian.semmer@u-psud.fr>
2020-05-15 12:50:31 +0200

    Ajout du code simulation de scan pour Kaiyuan

---
+++ Simulations/StageKaiyuan/SimuScan.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+d=300 #resolution du scan en nanomètre
+npx=100 # nombre de pixel du scan
+picture=np.zeros([npx,npx]) # fonction numpy qui initie un array de taille npx² avec des zeros
+sigma=300 # largeur caractéristique de la gaussienne
+nbspot=50 # nombe de particules dans le champs simulé
+'''___un petit morceau de code pour comprendre ce que signifie les indieces en numpy array_____'''
+picturei=np.zeros([npx,npx]) # pour voir quelle direction est influée par u ne modification du premierindice du tableau
+for i in range(100):
+    picturei[i,0:]=i
+picturej=np.zeros([npx,npx]) # pour voir quelle direction est influée par u ne modification du premierindice du tableau
+for j in range(100):
+    picturej[0:,j]=j
+fig=plt.figure(figsize=(10,20))
+
+plt.subplot(121)
+plt.imshow(picturei)
+
+plt.xlabel("variation du  premier indice d'un tableau python")
+plt.subplot(122)
+plt.imshow(picturej)
+plt.xlabel("variation du  duxième indice d'un tableau python")
+plt.show()
+'''_____________________________________________________________________________________________'''
+
+
+# In[7]:
+
+
+'''_________On ajoute les images des particules au tableau________'''
+tailleimage=12*int(sigma/d) # sigma/d correspond converti l'ecart des gaussiennes en pixel. tailleimage est le nombre de pixel que l'on utilise pour simuler une mage
+picture=np.zeros([npx,npx]) # on reinitialise le tableau  
+for i in picture : # bruit de fond 
+    i=np.random.normal(60,np.sqrt(60)) #60 est l'intensité moyenne du bruit de fond
+    
+for n in range (nbspot): #boucle for qui va ajouter chaque image de particules
+    In=np.random.normal(500,200) # on tire aléatoirement l'intensité et la position des particules
+    xn=np.random.randint(0,npx-6)
+    yn=np.random.randint(0,npx-6)
+    for gx in range(tailleimage):
+        for gy in range(tailleimage):
+            picture[xn+(gx-tailleimage//2),yn+(gy-tailleimage//2)]+=In*np.exp(-(((-3+gx)*300)**2+((-3+gy)*300)**2)/(2*sigma)**2)
+
+fig=plt.figure(figsize=(10,10))
+plt.imshow(picture)
+plt.title("scan simulé")
+plt.xlabel("un pixel=300nm")
+plt.colorbar()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
